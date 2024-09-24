319b3aa52fd5fa51442cae896b55859a9d5a4c9d
Buza Dan <buzadandeveloper@gmail.com>
2024-09-24 14:44:07 +0300

    feat: added middleware

--- src/features/store.jsx
+++ src/features/store.jsx
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import toDoReducer from "./toDoSlice";
+import { loggerMiddleware } from "./loggerMiddleware";
 
 export const store = configureStore({
   reducer: {
     toDo: toDoReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(loggerMiddleware),
 });
